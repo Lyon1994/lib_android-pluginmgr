32a782ff1dea7c3852eaf34a47355861b1d73188
Lyon1994 <765211630@qq.com>
2016-01-11 17:46:28 +0800

    update加入模块module-alipay

--- src/main/java/org/lyon_yan/app/android/lib_android_pluginmgr/ApkPluginManager.java
+++ src/main/java/org/lyon_yan/app/android/lib_android_pluginmgr/ApkPluginManager.java
@@ -23,20 +23,20 @@ public class ApkPluginManager {
      * @param context
      */
     public static void init(Context context, String... plugin_folder) {
-        if (androidx.pluginmgr.PluginManager.getSingleton() == null) {
             androidx.pluginmgr.PluginManager.init(context);
+            pluginLoaders.clear();
             for (String folder : plugin_folder) {
                 insertPluginFolder(folder, PluginLoader.create());
             }
-        }
     }
 
     /**
      * 插入插件目录进入扫描
+     *
      * @param folder
      * @param pluginLoader
      */
-    public static void insertPluginFolder(String folder, PluginLoader pluginLoader) {
+    protected static void insertPluginFolder(String folder, PluginLoader pluginLoader) {
         if (folder == null) {
             folder = PluginLoader.DEFAULT_PLUGIN_FOLDER;
         }
@@ -44,5 +44,39 @@ public class ApkPluginManager {
             return;
         }
         pluginLoaders.put(folder, PluginLoader.create(folder));
+    }
+
+    /**
+     * 插入插件目录
+     *
+     * @param folder
+     */
+    public static void insertPluginFolder(String... folder) {
+        for (String s : folder) {
+            pluginLoaders.put(s, PluginLoader.create(s));
+        }
+    }
+
+
+    /**
+     * 获取指定的插件加载器
+     *
+     * @param folder 插件的内部文件存储位置
+     * @return
+     */
+    public static PluginLoader getPluginLoader(String folder) {
+        if (pluginLoaders.containsKey(folder)) {
+            return pluginLoaders.get(folder);
+        }
+        return null;
+    }
+
+    /**
+     * 获取默认的插件加载器
+     *
+     * @return
+     */
+    public static PluginLoader getDefaultPluginLoader() {
+        return getPluginLoader(PluginLoader.DEFAULT_PLUGIN_FOLDER);
     }
 }
